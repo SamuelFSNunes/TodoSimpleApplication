0c747b09a42415e1b23f16489963e1dbe6e76403
SamuelFSNunes <samucasantana2003@gmail.com>
2024-05-23 14:20:19 -0300

    feat: add new mwthod
    added findByUsername using UserDetails class

--- todosimple/src/main/java/com/samuelnunes/todosimple/repositories/UserRepository.java
+++ todosimple/src/main/java/com/samuelnunes/todosimple/repositories/UserRepository.java
@@ -2,8 +2,10 @@ package com.samuelnunes.todosimple.repositories;
 
 import com.samuelnunes.todosimple.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByUsername(String username);
 }
